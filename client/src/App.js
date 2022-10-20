e80ebe55cf4b23ac3908ac5689da28c30d6f5203
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-10-20 10:17:58 +0200

    Add footer & navbar components and instal Sass

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,40 @@
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Route, Outlet } from "react-router-dom";
+import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Single from "./pages/Single";
 import Write from "./pages/Write";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
 
 const router = createBrowserRouter([
   {
     path:"/",
-    element:<div>
-    This is Home
-  </div>,
+    element:<Layout />,
+    children:[
+      {
+        path:"/",
+        element:<Home />
+      },
+      {
+        path:"/post/:id",
+        element:<Single />
+      },
+      {
+        path:"/write",
+        element:<Write />
+      }
+    ]
   },
   {
     path:"/login",
@@ -19,19 +44,14 @@ const router = createBrowserRouter([
     path:"/register",
     element:<Register />,
   },
-  {
-    path:"/single",
-    element:<Single />,
-  },
-  {
-    path:"/write",
-    element:<Write />,
-  },
 ]);
+
 function App() {
   return (
-    <div>
-      <RouterProvider router={router} />
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
     </div>
   );
 }
