5dac6fe071b68673a741783cfc41e2b1f49da9a0
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-10-20 15:06:26 +0200

    Add footer style

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,28 +1,47 @@
-import React from 'react';
-import {Link} from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
+    <div className="navbar">
       <div className="container">
         <div className="logo">
-          <img src={logo} alt="logo de Wendy Baqué, auteure, lectrice et chroniqueuse"/>
+          <img
+            src={logo}
+            alt="logo de Wendy Baqué, auteure, lectrice et chroniqueuse"
+          />
         </div>
         <div className="links">
-          <Link className='link' to="/?cat=massecritique"><h6>Masse Critique</h6></Link>
-          <Link className='link' to="/?cat=coupdecoeur"><h6>Coups de coeur</h6></Link>
-          <Link className='link' to="/?cat=feelgood"><h6>Feel-good</h6></Link>
-          <Link className='link' to="/?cat=romance"><h6>Romance</h6></Link>
-          <Link className='link' to="/?cat=thriller"><h6>Thriller</h6></Link>
-          <Link className='link' to="/?cat=divers"><h6>Divers</h6></Link>
+          <Link className="link" to="/?cat=massecritique">
+            <h6>Masse Critique</h6>
+          </Link>
+          <Link className="link" to="/?cat=coupdecoeur">
+            <h6>Coups de coeur</h6>
+          </Link>
+          <Link className="link" to="/?cat=feelgood">
+            <h6>Feel-good</h6>
+          </Link>
+          <Link className="link" to="/?cat=romance">
+            <h6>Romance</h6>
+          </Link>
+          <Link className="link" to="/?cat=thriller">
+            <h6>Thriller</h6>
+          </Link>
+          <Link className="link" to="/?cat=divers">
+            <h6>Divers</h6>
+          </Link>
           <span>Wendy</span>
           <span>Logout</span>
-          <span className='write'><Link to="/write" className='link'>Write</Link></span>
+          <span className="write">
+            <Link to="/write" className="link">
+              Write
+            </Link>
+          </span>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
