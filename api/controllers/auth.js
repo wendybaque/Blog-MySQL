b88cf10dbe4eda73edacce609341f601c445854a
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-10-21 12:08:07 +0200

    Commit before checkout

---
+++ api/controllers/auth.js
@@ -0,0 +1,28 @@
+import { db } from "../db.js";
+import bcrypt from "bcryptjs";
+
+export const register = (req, res) => {
+  // CHECK EXISTING USER
+  const query = "SELECT * FROM users WHERE email=? OR username=?";
+  db.query(q, [req.body.email, req.body.username], (err, data) => {
+    if (err) return res.json(err);
+    if (data.length) return res.status(409).json("User already exist.");
+
+    // HASH THE PASSWORD & CREATE A USER
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+
+    const query =
+      "INSERT INTO users(`username`, `email`, `password`) VALUES (?)";
+    const values = [req.body.username, req.body.email, hash];
+
+    db.query(q, [values], (err, data) => {
+      if (err) return res.json(err);
+      return res.status(200).json("User has been created");
+    });
+  });
+};
+
+export const login = (req, res) => {};
+
+export const logout = (req, res) => {};
