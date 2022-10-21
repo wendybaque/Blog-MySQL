a008504f66442f93920d5df98776a97cb8b3abae
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-10-21 12:07:39 +0200

    Commit before checkout

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Register() {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+  console.log(inputs)
+
   return (
     <div className="auth">
       <h1>Register</h1>
       <form>
-        <input type="text" placeholder="username" required/>
-        <input type="email" placeholder="email" required/>
-        <input type="password" placeholder="password" required/>
+        <input
+          type="text"
+          placeholder="username"
+          required
+          name="username"
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          placeholder="email"
+          required
+          name="email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          required
+          name="password"
+          onChange={handleChange}
+        />
         <button>Register</button>
         <p>This is an error</p>
         <span>
