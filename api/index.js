6ddb1a2ef717960d99025af6d7d73f6332396ee5
Wendy Baqué <99959128+wendybaque@users.noreply.github.com>
2022-10-21 11:15:23 +0200

    Add routes and controllers files and folders

--- api/index.js
+++ api/index.js
@@ -1,9 +1,27 @@
 import express from "express";
+import postRoutes from "./routes/posts.js";
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
 
 const app = express();
 
 app.use(express.json());
 
-app.listen(5000, () => {
-    console.log("Connected to backend.")
+// POSTS
+app.use("/api/posts", postRoutes)
+
+// USERS
+app.use("/api/users", userRoutes)
+
+// AUTH
+app.use("/api/auth", authRoutes)
+
+
+// BACKEND TESTS & CONFIG
+app.get("/test", (req, res) => {
+  res.json("It works !")
 })
+
+app.listen(5000, () => {
+  console.log("Connected to backend.");
+});
